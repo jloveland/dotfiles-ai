526c30bddf847d21768e5858dbe2f82b3dbc2924
jloveland <jason.c.loveland@gmail.com>
2025-06-09 20:22:20 -0400

    Adding nvm install

--- node/nvm.sh
+++ node/nvm.sh
@@ -4,5 +4,6 @@ source $(brew --prefix nvm)/nvm.sh
 # Tell nvm to use latest node version installed as default
 nvm alias default node
 
-export NODE_VERSION=v22
+export NODE_VERSION=v23
+nvm install $NODE_VERSION
 nvm use $NODE_VERSION
